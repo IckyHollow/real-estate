ce54d0cafd90c4945d62e556da972cb186c4cf3d
IckyHollow <chutienkhang@gmail.com>
2024-02-07 09:11:22 +0700

    Add new project real estate

---
+++ Project_Real_ estate/Controllers/AdminController.cs
@@ -0,0 +1,99 @@
+﻿using PagedList;
+using Project_Real__estate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net.Mail;
+using System.Net;
+
+namespace Project_Real__estate.Controllers
+{
+    public class AdminController : Controller
+    {
+        private projectEntities1 db = new projectEntities1();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if (Session["UserId"] != null)
+            {               
+                ViewBag.AdsCount = db.Advertisements.Count();
+                ViewBag.AgentCount = db.Agents.Count();
+                ViewBag.SellerCount = db.Sellers.Count();
+                ViewBag.ReportCount = db.Reports.Count();
+                //Activate
+                ViewBag.AdsActivateCount = db.Advertisements.Where(a=>a.isActivate == true).Count();
+                ViewBag.AgentActivateCount = db.Agents.Where(a => a.isActivate == true).Count();
+                ViewBag.SellerActivateCount = db.Sellers.Where(a => a.isActivate == true).Count();
+                //not activate
+                ViewBag.AdsNotActivateCount = db.Advertisements.Where(a => a.isActivate == false).Count();
+                ViewBag.AgentNotActivateCount = db.Agents.Where(a => a.isActivate == false).Count();
+                ViewBag.SellerNotActivateCount = db.Sellers.Where(a => a.isActivate == false).Count();
+
+
+                return View();
+                
+            }
+            else
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(string username, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var f_password = GetMD5(password);
+                var data = db.Users.Where(s => s.UserName.Equals(username) && s.Password.Equals(f_password)).ToList();
+
+                if (data.Count() > 0)
+                {
+                    Session["UserId"] = data.FirstOrDefault().UserId;
+                    Session["UserName"] = data.FirstOrDefault().UserName;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    ViewBag.Message = "Wrong username or password";
+                }
+            }
+            return View();
+        }
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+        
+
+    }
+}
---
+++ Project_Real_ estate/Controllers/ReportsController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Project_Real__estate.Models;
+
+namespace Project_Real__estate.Controllers
+{
+    public class ReportsController : BaseController
+    {
+        private projectEntities1 db = new projectEntities1();
+
+        // GET: Reports
+        public ActionResult Index()
+        {
+            var reports = db.Reports.Include(r => r.Advertisement).Include(r => r.Agent).Include(r => r.Seller);
+            return View(reports.ToList());
+        }
+
+        // GET: Reports/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Report report = db.Reports.Find(id);
+            if (report == null)
+            {
+                return HttpNotFound();
+            }
+            return View(report);
+        }
+
+        // GET: Reports/Create
+        public ActionResult Create()
+        {
+            ViewBag.AdsId = new SelectList(db.Advertisements, "adsId", "Tiltle");
+            ViewBag.AgentId = new SelectList(db.Agents, "AgentId", "AgentName");
+            ViewBag.SellerId = new SelectList(db.Sellers, "SellerId", "Name");
+            return View();
+        }
+
+        // POST: Reports/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReportId,ReportDate,AdsId,SellerId,AgentId,Price")] Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reports.Add(report);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AdsId = new SelectList(db.Advertisements, "adsId", "Tiltle", report.AdsId);
+            ViewBag.AgentId = new SelectList(db.Agents, "AgentId", "AgentName", report.AgentId);
+            ViewBag.SellerId = new SelectList(db.Sellers, "SellerId", "Name", report.SellerId);
+            return View(report);
+        }
+
+        // GET: Reports/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Report report = db.Reports.Find(id);
+            if (report == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AdsId = new SelectList(db.Advertisements, "adsId", "Tiltle", report.AdsId);
+            ViewBag.AgentId = new SelectList(db.Agents, "AgentId", "AgentName", report.AgentId);
+            ViewBag.SellerId = new SelectList(db.Sellers, "SellerId", "Name", report.SellerId);
+            return View(report);
+        }
+
+        // POST: Reports/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReportId,ReportDate,AdsId,SellerId,AgentId,Price")] Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(report).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AdsId = new SelectList(db.Advertisements, "adsId", "Tiltle", report.AdsId);
+            ViewBag.AgentId = new SelectList(db.Agents, "AgentId", "AgentName", report.AgentId);
+            ViewBag.SellerId = new SelectList(db.Sellers, "SellerId", "Name", report.SellerId);
+            return View(report);
+        }
+
+        // GET: Reports/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Report report = db.Reports.Find(id);
+            if (report == null)
+            {
+                return HttpNotFound();
+            }
+            return View(report);
+        }
+
+        // POST: Reports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Report report = db.Reports.Find(id);
+            db.Reports.Remove(report);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult SendEmail(int? id)
+        {
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            Report report = db.Reports.Find(id);
+            if (report == null)
+            {
+                return HttpNotFound();
+            }
+            return View(report);
+        }
+        [HttpPost]
+        public ActionResult SendEmail(int id, string subject, string message)
+        {
+            try
+            {
+                string receiver = "";
+                if (ModelState.IsValid)
+                {
+                    Report find = db.Reports.Find(id);
+                    if (find.SellerId != null)
+                    {
+                        receiver = find.Seller.Email;
+                    }
+                    else if (find.AgentId != null)
+                    {
+                        receiver = find.Agent.Email;
+                    }
+                    var senderEmail = new MailAddress("nguyenhuytuan04@gmail.com", "Dream House Estate");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "Tuankun.1301";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = "Cảnh báo bài viết \""+find.Advertisement.Tiltle+"\" của tài khoản "+receiver+" sắp hết hạn bài đăng. \nBạn có muốn gia hạn cho bài đăng không?\n"+body
+                    })
+                    {
+                        smtp.Send(mess);
+                        ViewBag.Mes = "Gửi thành công";
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Mes = "Không thể gửi ";
+            }
+            return View();
+        }
+    }
+}
